1ae117329f8dc319d95257a157b0d355b93a9538
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-10-03 15:32:35 -0700

    up

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,22 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.+)\] \[to:(.+)\] \[flags:(.+\d)\] /).join(",")
+
+# Vérification de la présence d'un argument en ligne de commande
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <texte>"
+  exit 1
+end
+
+# Stockage de l'argument de ligne de commande dans une variable
+log_content = ARGV[0]
+
+# Définition du motif d'expression régulière
+pattern = /\[from:(.+)\] \[to:(.+)\] \[flags:(.+\d)\] /
+
+# Utilisation du motif pour trouver des correspondances dans le texte
+matches = log_content.scan(pattern)
+
+# Jointure des correspondances en une seule chaîne avec des virgules comme séparateurs
+result = matches.join(",")
+
+# Affichage du résultat
+puts result
