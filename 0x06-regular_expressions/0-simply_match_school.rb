4e15fe479fc343a38d046f769fc8fdff4d8e64f6
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-10-03 13:26:09 -0700

    up

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+# Check if there is one argument provided
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <text>"
+  exit 1
+end
+
+text_to_search = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /School/
+
+# Use the pattern to find matches in the input text
+matches = text_to_search.scan(pattern)
+
+# Print the matches (if any)
+if !matches.empty?
+  matches.each do |match|
+    puts match
+  end
+else
+  puts ""
+end
