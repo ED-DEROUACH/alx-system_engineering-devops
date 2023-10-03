ee2fb24c5105db40f0d0e76f8e63b05fbb3b8337
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-10-03 14:08:48 -0700

    up

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+# Check if there is one argument provided
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <text>"
+  exit 1
+end
+
+text_to_search = ARGV[0]
+
+# Define the regular expression pattern to match the provided cases
+pattern = /hb{0,1}tn/
+
+# Use the pattern to find matches in the input text
+matches = text_to_search.scan(pattern)
+
+# Print the matches (if any)
+if !matches.empty?
+  matches.each do |match|
+    puts match
+  end
+else
+  puts ""
+end
