caf14051e7768979d01f47cac6a4c6d557ae96f6
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-10-03 14:30:35 -0700

    up

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,24 +1,22 @@
 #!/usr/bin/env ruby
 
-# Check if there is one argument provided
+# Vérification de la présence d'un argument en ligne de commande
 if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <text>"
+  puts "Usage: #{$PROGRAM_NAME} <texte>"
   exit 1
 end
 
-text_to_search = ARGV[0]
+# Stockage de l'argument de ligne de commande dans une variable
+texte_a_rechercher = ARGV[0]
 
-# Define the regular expression pattern to match the provided cases
-pattern = /hb?tn/
+# Définition du motif de l'expression régulière
+motif = /hb?tn/
 
-# Use the pattern to find matches in the input text
-matches = text_to_search.scan(pattern)
+# Utilisation de la méthode scan pour trouver toutes les correspondances dans le texte d'entrée
+correspondances = texte_a_rechercher.scan(motif)
 
-# Print the matches (if any)
-if !matches.empty?
-  matches.each do |match|
-    puts match
-  end
-else
-  puts ""
-en
+# Jointure des correspondances en une seule chaîne de caractères
+resultat = correspondances.join
+
+# Affichage du résultat
+puts resultat
