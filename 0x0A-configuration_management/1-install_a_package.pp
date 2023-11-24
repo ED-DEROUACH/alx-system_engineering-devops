35ab5eb08e820bbf6f93eb4d9e06edcc065882e2
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-11-24 15:13:05 -0800

    up

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,24 +1,5 @@
-# 1-install_a_package.pp
-
-class { 'python': }
-
-package { 'python3-pip':
-  ensure => 'installed',
+# Using puppet to install flask from pip3
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
-
-package { 'virtualenv':
-  ensure => 'installed',
-}
-
-exec { 'create_virtualenv':
-  command     => '/usr/bin/virtualenv /opt/venv',
-  refreshonly => true,
-  creates     => '/opt/venv',
-}
-
-exec { 'install_flask':
-  command     => '/opt/venv/bin/pip3 install flask==2.1.0',
-  refreshonly => true,
-  require     => [Package['virtualenv'], Exec['create_virtualenv']],
-}
-
